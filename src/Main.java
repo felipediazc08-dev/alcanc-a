2004306d5e3c1e92b9e6babb6aafa93edc3dec2d
AndresD <felipediazc08@gmail.com>
2026-03-05 14:47:30 -0500

    Alcancía

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,103 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner teclado=new Scanner (System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        int m1, m2, m3, m4, m5;
+        int m20 = 0, m50 = 0, m100 = 0, m200 = 0, m500 = 0, m1000 = 0;
+        int opcion,total1,meta;
+        do{
+        System.out.println("""
+                Alcnacía
+                Seleccione un numero para realizar la opcion que quiera
+                1.Ahorrar
+                2 Contador de monedas
+                3 Valor acumulado 
+                4 Total del dinero   
+                5 Meta de ahorro
+                :
+                """);
+        opcion = teclado.nextInt();
+        switch (opcion) {
+            case 1:
+                System.out.println("""
+                        Ingrese la denominacion de la moneda
+                        1.20
+                        2.50
+                        3.100
+                        4.200
+                        5.500
+                        6.1000
+                        """);
+                m1 = teclado.nextInt();
+                switch (m1) {
+                    case 1:
+                        System.out.println("A ingresado una moneda de 20");
+                        m20 += 1;
+                        break;
+                    case 2:
+                        System.out.println("A ingresado una moneda de 50");
+                        m50 += 1;
+                        break;
+                    case 3:
+                        System.out.println("A ingresado una moneda de 100");
+                        m100 += 1;
+                        break;
+                    case 4:
+                        System.out.println("A ingresado una moneda de 200");
+                        m200 += 1;
+                        break;
+                    case 5:
+                        System.out.println("A ingresado una moneda de 500");
+                        m500 += 1;
+                        break;
+                    case 6:
+                        System.out.println("A ingresado una moneda de 1000");
+                        m1000 += 1;
+                        break;
+                    default:
+                        System.out.println("opcion no valida");
+
+                }
+
+                case 2:
+                System.out.println("la cantidad de monedas contadas es:");
+                System.out.println("denominacion de 20: "+m20);
+                System.out.println("denominacion de 50: "+m50);
+                System.out.println("denominacion de 100: "+m100);
+                System.out.println("denominacion de 200: "+m200);
+                System.out.println("denominacion de 500: "+m500);
+                System.out.println("denominacion de 1000: "+m1000);
+                System.out.println(" ");
+                break;
+            case 3:
+                System.out.println("el valor acumulado de cada denominacion es:");
+                System.out.println("20 : "+(m20*20));
+                System.out.println("50 : "+(m50*50));
+                System.out.println("100 : "+(m100*100));
+                System.out.println("200 : "+(m200*200));
+                System.out.println("500 : "+(m500*500));
+                System.out.println("1000 : "+(m1000*1000));
+                break;
+            case 4:
+                total1=(m20*20)+(m50*50)+(m100*100)+(m200*200)+(m500*500)+(m1000*1000);
+                System.out.println("su valor total es de: "+total1);
+                break;
+            case 5:
+                System.out.println("cual es su meta de ahorro ");
+                meta= teclado.nextInt();
+
+
+
+
+
         }
+
+        }while (opcion!=5);
+
     }
 }
